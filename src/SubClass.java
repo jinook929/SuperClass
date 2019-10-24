32907e7a38c1aafd4196d2652c87cf11d194f36a
jinook929 <jinook929@gmail.com>
2019-10-23 20:54:33 -0400

    Initial commit

---
+++ src/SubClass.java
@@ -0,0 +1,7 @@
+public class SubClass extends SuperClass {
+    @Override
+    public void printMethod() {
+        super.printMethod();
+        System.out.println("Printed in Subclass");
+    }
+}
---
+++ src/SuperClass.java
@@ -0,0 +1,7 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class SuperClass {
+    public void printMethod() {
+        System.out.println("Printed in Superclass");
+    }
+}
